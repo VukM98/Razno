27dfc8cdb6fd916b921ebf1e78c7435429b6b20b
VukM98 <149973946+VukM98@users.noreply.github.com>
2023-11-14 22:31:37 +0100

    Add files via upload

---
+++ Povrsina Kruga/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PovršinaKruga
+{
+    delegate String Product(int r);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Product PovKruga = (r) => Convert.ToString(r * r * Math.PI);
+            Console.WriteLine("Result is : " + PovKruga(10));
+        }
+    }
+}
