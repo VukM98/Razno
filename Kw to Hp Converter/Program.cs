3d5b9517cc924905aa88870cf21e46f7c0e25782
VukM98 <149973946+VukM98@users.noreply.github.com>
2023-11-14 22:36:48 +0100

    Add files via upload

---
+++ Kw to Hp Converter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace MyApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            double result = 0;
+
+            Console.WriteLine("Please, enter value: ");
+            double value = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Convert to (enter kw or hp):");
+            string convertTo = Console.ReadLine();
+
+            if (convertTo == "kw")
+            {
+                result = KwHpConverter(value, "kw");
+            }
+            else
+            {
+                result = KwHpConverter(value, "hp");
+            }
+
+            Console.WriteLine(result);
+
+        }
+
+        public static double KwHpConverter(double inputValue, string convertTo)
+        {
+            if (convertTo == "hp")
+            {
+                return inputValue / 0.745699872;
+            }
+            else
+            {
+                return inputValue * 0.745699872;
+            }
+        }
+
+    }
+
+}
